4209bb04a17c6813e92c7ad514f1710d8f620534
aakashagrwl <akashag00786@gmail.com>
2024-02-10 22:20:52 +0530

    added 3 new functions in ml_model python code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,13 @@
 
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding code for Model1")
+
+
+def model2():
+    print("Adding code for Model2")
+
+
+def model3():
+    print("Adding code for Model3")
